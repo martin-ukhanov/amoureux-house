3b84102e02ee2b3af01b64f676b29f66e4b1860f
Martin Ukhanov <martin.ukhanov@gmail.com>
2025-01-28 00:34:39 -0500

    Added Lenis

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
 	import '../app.css';
+	import 'lenis/dist/lenis.css';
+	import Lenis from 'lenis';
 	import Header from '$lib/components/Header.svelte';
 	import Hero from '$lib/components/sections/Hero.svelte';
 	import Story from '$lib/components/sections/Story.svelte';
+
+	$effect(() => {
+		// Initialize Lenis
+		const lenis = new Lenis();
+
+		function raf(time: number) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	});
 </script>
 
 <Header />
