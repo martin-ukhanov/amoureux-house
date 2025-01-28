b11a87fd2cba655435df68a470e650defbd059e6
Martin Ukhanov <martin.ukhanov@gmail.com>
2025-01-28 17:05:07 -0500

    Added new breakpoint

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -4,34 +4,33 @@
 </script>
 
 <section
-	class="m-6 grid grid-cols-[auto_1fr] grid-rows-[repeat(5,auto)] gap-4 border-b-[1px] border-primary pb-4 desktop:grid-cols-[auto_auto_1fr] desktop:grid-rows-[1fr_auto_auto]"
+	class="m-6 grid grid-cols-[auto_1fr] grid-rows-[repeat(5,auto)] gap-4 border-b-[1px] border-primary pb-4 tablet:grid-cols-[auto_auto_1fr] tablet:grid-rows-[1fr_auto_auto]"
 >
 	<h1 class="sr-only">The Amoureux House</h1>
 
-	<span aria-hidden="true" class="-mb-[0.146em] block self-end text-h1 max-desktop:row-start-1">
+	<span aria-hidden="true" class="-mb-[0.146em] block self-end text-h1 max-tablet:row-start-1">
 		The
 	</span>
 	<img
 		src={loopingVideo}
 		alt="Looping Video"
-		class="col-span-2 rounded-2xl shadow-lg shadow-[black]/50 max-desktop:row-start-4"
+		class="col-span-2 rounded-2xl shadow-lg shadow-[black]/50 max-tablet:row-start-4"
 	/>
 
 	<span
 		aria-hidden="true"
-		class="col-span-full mt-[0.02333em] mr-[0.05em] -mb-[0.146em] block text-right text-h1 max-desktop:row-start-2"
+		class="col-span-full mt-[0.02333em] mr-[0.05em] -mb-[0.146em] block text-right text-h1 max-tablet:row-start-2"
 	>
 		Amoureux
 	</span>
 
-	<p class="self-end text-body-sm max-desktop:row-start-5 max-desktop:-mt-2 max-desktop:ml-2">
+	<p class="self-end text-body-sm max-tablet:row-start-5 max-tablet:-mt-2 max-tablet:ml-2">
 		Featuring Pelagie X<br />A film by Breakwater Studios
 	</p>
-	<span
-		aria-hidden="true"
-		class="mt-[0.02333em] -mb-[0.146em] block text-h1 max-desktop:row-start-3">House</span
+	<span aria-hidden="true" class="mt-[0.02333em] -mb-[0.146em] block text-h1 max-tablet:row-start-3"
+		>House</span
 	>
-	<div class="relative max-desktop:row-start-3">
+	<div class="relative max-tablet:row-start-3">
 		<div class="button-position absolute">
 			<ButtonCircle text="Visit The House" href="/" />
 		</div>
