4857c371430b607f7f220117164f7c9015fedbb4
Martin Ukhanov <martin.ukhanov@gmail.com>
2025-01-28 18:01:53 -0500

    Created header component layout

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,1 +1,13 @@
-<header>Header</header>
+<header
+	class="pointer-events-none sticky top-0 left-0 z-10 flex w-full p-6 max-tablet:justify-end tablet:fixed"
+>
+	<nav class="hidden flex-col items-start gap-y-0.5 tablet:flex">
+		<a href="/" class="pointer-events-auto">Watch the film</a>
+		<a href="/" class="pointer-events-auto">Pelagie's Story</a>
+		<a href="/" class="pointer-events-auto">Timeline</a>
+		<a href="/" class="pointer-events-auto">The House</a>
+		<a href="/" class="pointer-events-auto">Resources</a>
+	</nav>
+
+	<button class="tablet:hidden">Menu</button>
+</header>
