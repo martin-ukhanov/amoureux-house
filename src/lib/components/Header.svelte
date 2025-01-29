3bcbdbe36fad1435d9143c87b785c4e016c56dc6
Martin Ukhanov <martin.ukhanov@gmail.com>
2025-01-28 20:42:17 -0500

    Set fixed header height on mobile

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header
-	class="pointer-events-none sticky top-0 left-0 z-10 flex w-full px-6 pt-6 max-tablet:justify-end tablet:fixed"
+	class="pointer-events-none fixed top-0 left-0 z-10 flex w-full items-end px-6 max-tablet:h-[var(--header-height)] max-tablet:justify-end tablet:pt-6"
 >
 	<nav class="hidden flex-col items-start gap-y-0.5 tablet:flex">
 		<a href="/" class="pointer-events-auto">Watch the film</a>
@@ -11,3 +11,11 @@
 
 	<button class="tablet:hidden">Menu</button>
 </header>
+
+<style>
+	@reference '../../app.css';
+
+	:root {
+		--header-height: calc(var(--spacing) * 10);
+	}
+</style>
